64c6bd906b12e8386556c8a45f36c084e05936e9
Marco Jansen <jansen.marco95@gmail.com>
2014-10-22 15:41:09 +0200

    w

--- javascript.js
+++ javascript.js
@@ -2,13 +2,11 @@ $(document).ready(Function() {
         $.ajax({
             type: "Post",
             beforeSend: function(request) {
-            request.setRequestHeader("Accept","application/json")
+                request.setRequestHeader("Accept", "application/json")
             },
             url: "http://localhost:8080/NotFlix/resources/movies",
             dataType: "json",
         }).fail(function(jqXHR, textStatus) {
             alert("API Request failed: " + textStatus);
-        }).done(function(data) {
-            $(
-        });
+        }).done(function(data) {});
     };
